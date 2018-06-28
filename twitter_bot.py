15c627f279ad63a5d1618720e86e5369fb4979ab
Chris <ninja1511@gmail.com>
2018-06-28 19:28:40 +0100

    Add files via upload

---
+++ twitter_bot.py
@@ -0,0 +1,5 @@
+import twitter
+api = twitter.Api(consumer_key='nYNeK5nLq0jvl4HRCMlfLR1a3',
+                  consumer_secret='UHRcPBS8u28Tbcge3QxiUh4T7HzLsRide7bOeE7yGGBFyVJg3x',
+                  access_token_key='2356120214-ULUNWcqQX8wNyKuf1VLxEv8VdQAGwj8vRMnlQHH',
+                  access_token_secret='aEyphB0CJzw3xRN3QEqmH9LNleoyX55Eg7nyNFiNRrSAo')
